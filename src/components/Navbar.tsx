fe92377f906f21716f855dab4a74bcbd032a35fb
beawkiattisak <beawkiattisak@gmail.com>
2024-09-27 18:56:28 +0700

    WTF Fixing multiple time in 1 hour kuy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-[#285577] w-full h-6 px-10">
+    <div className="bg-[#285577] w-full h-6 px-6 sm:px-10">
         <span className='text-white'>
             /about/beawkiattisak
         </span>
